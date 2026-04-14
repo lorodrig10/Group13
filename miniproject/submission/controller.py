2700cd2c6bc53379cc2dee966aa7ff8e9dfe781b
Nathiboy51 <nathankackkack@gmail.com>
2026-04-14 15:18:17 +0200

    level 0

--- miniproject/submission/controller.py
+++ miniproject/submission/controller.py
@@ -12,7 +12,23 @@ class Controller:
     def step(self, sim: MiniprojectSimulation):
         # implement your control algorithm here
         olfaction = sim.get_olfaction(sim.fly.name)
+        
+        left_antenna = olfaction[0, 0]
+        right_antenna = olfaction[1, 0]
+        left_palp = olfaction[2, 0]
+        right_palp = olfaction[3, 0]
+
+        # For a simple steering controller, you might just average the left side 
+        # and average the right side to decide which way to turn:
+        left_signal = left_antenna + left_palp
+        right_signal = right_antenna + right_palp
+        # print(f"Olfaction: {olfaction}", f"Left signal: {left_signal}", f"Right signal: {right_signal}")
         # get other observations as needed
-        drives = np.array([1.0, 1.0])  # replace with your control logic
+        if left_signal > right_signal:
+            # turn left
+            drives = np.array([0.2, 1.0])
+        elif right_signal > left_signal:
+            # turn right
+            drives = np.array([1.0, 0.2])
         joint_angles, adhesion = self.turning_controller.step(drives)
         return joint_angles, adhesion
